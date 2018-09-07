a071272489cd16e30502f7f7a0cd2bb684432311
christine gomez <christine.gomez1226@gmail.com>
2018-09-07 02:59:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 dennis_ritchies_language.to_s = programmer_hash[:dennis_ritchie][:languages]
+		 dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
